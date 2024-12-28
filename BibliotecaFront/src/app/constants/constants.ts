b57de63e7a5b27c5934c3edd536ed9631e21b154
Andres Puerta <andres-felipe027@hotmail.com>
2024-12-27 19:21:52 -0500

    Mi primer commit

---
+++ BibliotecaFront/src/app/constants/constants.ts
@@ -0,0 +1,26 @@
+export const constValues = {
+  //Mensajes
+
+  librocreadocorrectamente: "El libro se creó correctamente",
+  libromodificadocorrectamente: "El libro se modificó correctamente",
+
+  //Labels
+
+   //Constantes para presentar mensajes al usuario
+   exito: "Éxito",
+  advertencia: "Advertencia",
+  error: "Error",
+
+  //Botones
+  agregar: "Agregar",
+  si: "Si",
+  no: "No",
+  aceptar: "Aceptar",
+  cancelar: "Cancelar",
+  filtros: "Filtros",
+  buscar: "Buscar",
+  volver: "Volver",
+  guardar: "Guardar",
+  modificar: "Modificar",
+  nuevo: "Nuevo"
+}
